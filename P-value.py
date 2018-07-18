0e8a85b3d70db7c7858a2d1439a818dbb82b9ee1
Lucas Iervolino <lucas.iervolino@gmail.com>
2018-07-18 18:07:30 -0300

    P-value stats test

--- P-value.py
+++ P-value.py
@@ -1,15 +1,6 @@
-#p-value estimation o
+#p-value statistical test to obtain significance of independent variables on explaining response variable y
 import statsmodels.api as sm
 
-est = sm.OLS(dataset_train_y, dataset_train_x)
+est = sm.OLS(y, x)
 est2 = est.fit()
 print(est2.summary())
-
-
-
-
-
-
-
-
-
